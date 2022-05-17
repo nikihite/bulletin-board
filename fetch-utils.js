0ff92a02a7b12c306070590cc19864625877e610
Niki Hite <nikihite@comcast.net>
2022-05-17 11:39:26 -0700

    sign up user and sign in user showing up on page

---
+++ fetch-utils.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://zcwghasajjcnjalirtgv.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpjd2doYXNhampjbmphbGlydGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTg5MTEsImV4cCI6MTk2Nzg3NDkxMX0.PABjBwN_OcjQPMnybnEw2Gb1TCQAlLw_oz348jM0rSw';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
